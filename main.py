f62a6076842de489965cfac61bfc43df85156eef
Recio555 <hnorecio@gmail.com>
2025-07-15 20:55:30 -0400

    A_2

--- main.py
+++ main.py
@@ -7,36 +7,35 @@ from bson import ObjectId
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+
+# Initialize FastAPI app
+uri = "mongodb+srv://hnorecio:Filipenses48@cluster0.xiwaxdd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global client, database, collection
+    
+    client = MongoClient(uri)
+    # Ensure the database is available:
+    database = client["sample_mflix"]
+    pong = database.command("ping")
+    if int(pong["ok"]) != 1:
+        raise Exception("Cluster connection is not okay!")
+
+    todo_lists = database.get_collection('comments')
+    
+    #app.todo_dal = ToDoDAL(todo_lists)
+
+    # Yield back to FastAPI Application:
+    yield
+
+    # Shutdown:
+    client.close()
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"mensaje": "Probando"}
 
-uri = "mongodb+srv://reciosalvador:En8A21YCVUfUgUfC@cluster0.fnhcurd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-# Seleccionar la base de datos (se crea si no existe)
-db = client["Informedb"]
-
-# Seleccionar la colección (se crea si no existe)
-coleccion = db["publicador"]
-
-
-nuevo_publicador = {
-    "nombre": "Pedro",
-    "horas": 12,
-    "revisitas": 5
-}
-
-resultado = coleccion.insert_one(nuevo_publicador)
-print("ID insertado:", resultado.inserted_id)
 
  
