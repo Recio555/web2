9e6ba2be3882f244b78c9f9af8d7e57e0157d6ef
Recio555 <hnorecio@gmail.com>
2025-07-21 12:20:19 -0400

    actualizar

--- db.py
+++ db.py
@@ -2,14 +2,10 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 uri = "mongodb+srv://reciosalvador:En8A21YCVUfUgUfC@cluster0.fnhcurd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-# Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
-    #print("Pinged your deployment. You successfully connected to MongoDB!")
+
 except Exception as e:
     print(e)
 
